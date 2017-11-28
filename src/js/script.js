48fd020ddd2022bf66e53a36235b7a84b483ec34
Julie Van Beethoven <jvb@Julies-MacBook-Pro.local>
2017-11-28 10:45:24 +0100

    :sparkles: test with three.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,34 @@
 import synth from './lib/synth';
 import * as THREE from 'three';
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+const mesh = new THREE.Mesh(geometry, material);
+const renderer = new THREE.WebGLRenderer({antialias: true});
+
 const init = () => {
 
   console.log(`Hello`);
   synth();
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-  const renderer = new THREE.WebGLRenderer();
+  camera.position.z = 1;
+  scene.add(mesh);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({color: 0x00ffff});
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  camera.position.z = 5;
-
-  animate(cube, renderer, scene, camera);
+  animate();
 
 };
 
-const animate = (cube, renderer, scene, camera) => {
+const animate = () => {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
   renderer.render(scene, camera);
 };
 
