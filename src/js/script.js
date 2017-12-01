ce43361036a6e9bb80cf370cfd8564c7cb152744
jurgenpeuteman <jurgen.peuteman@student.howest.be>
2017-12-01 12:18:13 +0100

    assets loading 2

--- src/js/script.js
+++ src/js/script.js
@@ -6,27 +6,19 @@ const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerH
 
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const cylindergeometry = new THREE.CylinderGeometry(.2, .2, .2, .2);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-//const material2 = new THREE.MeshBasicMaterial();
+//const material2 = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 const mesh2 = new THREE.Mesh(cylindergeometry, material);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 const loader = new THREE.JSONLoader();
 
-let potgoud;
+let potgoud, kabouter, fakkel, paddestoel, boomstronk, pickaxe;
+
 
 const init = () => {
 
-  loader.load(`./assets/json/potgoud.json`, function (geometry) {
-    potgoud = new THREE.Mesh(geometry, material);
-
-    potgoud.scale.set(0.01, 0.01, 0.01);
-    potgoud.position.x = 0;
-    potgoud.rotation.y = 2;
-    scene.add(potgoud);
-    animate();
-  });
-
+  loadAssets();
   console.log(`Hello`);
   synth();
   camera.position.z = 5;
@@ -39,8 +31,68 @@ const init = () => {
 
 };
 
-const animate = () => {
-  requestAnimationFrame(animate);
+const loadAssets = () => {
+  //POTGOUD
+  loader.load(`./assets/json/potgoud.json`, function (geometry) {
+    potgoud = new THREE.Mesh(geometry, material);
+    potgoud.scale.set(0.001, 0.001, 0.001);
+    potgoud.position.x = 0;
+    potgoud.rotation.x = 46;
+    scene.add(potgoud);
+  });
+
+  //kabouter
+  loader.load(`./assets/json/kabouter.json`, function (geometry) {
+    kabouter = new THREE.Mesh(geometry, material);
+    kabouter.scale.set(0.001, 0.001, 0.001);
+    kabouter.position.x = 4;
+    kabouter.rotation.x = 0;
+    scene.add(kabouter);
+  });
+
+  //fakkel
+  loader.load(`./assets/json/fakkel.json`, function (geometry) {
+    fakkel = new THREE.Mesh(geometry, material);
+    fakkel.scale.set(0.001, 0.001, 0.001);
+    fakkel.position.x = 2;
+    fakkel.rotation.x = 0;
+    scene.add(fakkel);
+  });
+
+  //paddestoel
+  loader.load(`./assets/json/paddestoel.json`, function (geometry) {
+    paddestoel = new THREE.Mesh(geometry, material);
+    paddestoel.scale.set(0.001, 0.001, 0.001);
+    paddestoel.position.x = 4;
+    paddestoel.rotation.x = 0;
+    scene.add(paddestoel);
+  });
+
+  //boomstronk
+  loader.load(`./assets/json/boomstronk.json`, function (geometry) {
+    boomstronk = new THREE.Mesh(geometry, material);
+    const s = 0.006;
+    boomstronk.scale.set(s, s, s);
+    boomstronk.position.x = 3;
+    boomstronk.rotation.x = 0;
+    scene.add(boomstronk);
+  });
+
+  //pickaxe
+  loader.load(`./assets/json/pickaxe.json`, function (geometry) {
+    pickaxe = new THREE.Mesh(geometry, material);
+    pickaxe.scale.set(0.001, 0.001, 0.001);
+    pickaxe.position.x = 4;
+    pickaxe.rotation.x = 0;
+    scene.add(pickaxe);
+  });
+
+  render();
+};
+
+const render = () => {
+  requestAnimationFrame(render);
+  scene.rotation.x += 0.01;
   /*
   mesh2.rotation.y += 0.01;
   mesh2.rotation.x += 0.01;
