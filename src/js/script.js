5754497482119b9d98c197f4b39b903c94682129
Julie Van Beethoven <jvb@Julies-MacBook-Pro.local>
2017-12-05 16:53:48 +0100

    Minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,61 @@
 import synth from './lib/synth';
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+let potgoud, kabouter, fakkel, paddestoel, boomstronk, pickaxe, container,
+  scene, camera, WIDTH, HEIGHT;
 
+const renderer = new THREE.WebGLRenderer({antialias: true});
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const cylindergeometry = new THREE.CylinderGeometry(.2, .2, .2, .2);
-//const material2 = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
 const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 const mesh2 = new THREE.Mesh(cylindergeometry, material);
-const renderer = new THREE.WebGLRenderer({antialias: true});
+
 const loader = new THREE.JSONLoader();
-
-let potgoud, kabouter, fakkel, paddestoel, boomstronk, pickaxe;
-
 
 const init = () => {
 
+  createScene();
   loadAssets();
+  loadKabouter();
   console.log(`Hello`);
-  synth();
-  camera.position.z = 5;
+  // synth();
+  render();
+
+};
+
+const createScene = () => {
+
+  container = document.createElement(`div`);
+  document.body.appendChild(container);
+
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+  4,
+  WIDTH / HEIGHT,
+  0.11,
+  500
+  );
+
+  camera.position.set(0, 0, 100);
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
+  container.appendChild(renderer.domElement);
   mesh2.position.x = 2;
 
   scene.add(mesh2);
   scene.add(mesh);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  window.addEventListener(`resize`, handleWindowResize, false);
 
 };
 
@@ -37,18 +65,11 @@ const loadAssets = () => {
     potgoud = new THREE.Mesh(geometry, material);
     potgoud.scale.set(0.001, 0.001, 0.001);
     potgoud.position.x = 0;
-    potgoud.rotation.x = 46;
+    potgoud.rotation.x = 3;
     scene.add(potgoud);
   });
 
-  //kabouter
-  loader.load(`./assets/json/kabouter.json`, function (geometry) {
-    kabouter = new THREE.Mesh(geometry, material);
-    kabouter.scale.set(0.001, 0.001, 0.001);
-    kabouter.position.x = 4;
-    kabouter.rotation.x = 0;
-    scene.add(kabouter);
-  });
+
 
   //fakkel
   loader.load(`./assets/json/fakkel.json`, function (geometry) {
@@ -86,21 +107,60 @@ const loadAssets = () => {
     pickaxe.rotation.x = 0;
     scene.add(pickaxe);
   });
+};
 
-  render();
+const loadKabouter = () => {
+  // kabouter
+  // loader.load(`./assets/json/kabouter.json`, function (geometry) {
+  //   kabouter = new THREE.Mesh(geometry, material);
+  //   kabouter.scale.set(0.001, 0.001, 0.001);
+  //   kabouter.position.x = 4;
+  //   kabouter.rotation.x = 0;
+  //   scene.add(kabouter);
+  // });
+
+  loader.load(`./assets/json/kabouter.json`, loadGnome);
+};
+
+const loadGnome = () => {
+
+  kabouter = new THREE.Mesh(geometry, material);
+  kabouter.scale.set(0.001, 0.001, 0.001);
+  kabouter.position.x = 4;
+  kabouter.rotation.x = 0;
+  scene.add(kabouter);
+  console.log(loader.onload);
+
+};
+
+const checkCollision = () => {
+
+  console.log(fakkel);
+
+  // const gnomePos = kabouter.position;
+  // const paddestoelPos = paddestoel.position;
+  //
+  // const distance = gnomePos.distanceTo(paddestoelPos);
+  // if (distance < 12) {
+  //   console.log(`boom`);
+  synth();
+  //
+  // }
+
 };
 
 const render = () => {
-  requestAnimationFrame(render);
-  scene.rotation.x += 0.01;
-  /*
-  mesh2.rotation.y += 0.01;
-  mesh2.rotation.x += 0.01;
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
-  */
-
   renderer.render(scene, camera);
+  checkCollision();
+
+  requestAnimationFrame(render);
+};
+
+const handleWindowResize = () => {
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+
 };
 
 init();
