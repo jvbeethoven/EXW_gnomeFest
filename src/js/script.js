7863005e2ffcdf027ac666cda03df5a02b1c7121
Julie Van Beethoven <jvb@Julies-MacBook-Pro.local>
2017-12-07 15:52:47 +0100

    Style changes, camera update. Needs responsive fix

--- src/js/script.js
+++ src/js/script.js
@@ -22,6 +22,7 @@ let lastSynthTriggerdTime = 0;
 const init = () => {
 
   createScene();
+  addText();
   loadAssets()
     .then(() => render())
     .then(() => makeDraggable());
@@ -41,14 +42,13 @@ const createScene = () => {
   scene = new THREE.Scene();
   // scene.background = new THREE.Color(0xff0000);
 
-  camera = new THREE.PerspectiveCamera(
-  4,
-  WIDTH / HEIGHT,
-  0.11,
-  500
-  );
+  camera = new THREE.OrthographicCamera(WIDTH / - 2, WIDTH / 2, HEIGHT / 2, HEIGHT / - 2, 1, 100);
+  // camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 1, 10000);
 
-  camera.position.set(0, 0, 100);
+  camera.position.set(- 10, 5, 100);
+  camera.rotation.X = 0;
+  camera.rotation.y = 0;
+  camera.rotation.z = 0;
 
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -62,8 +62,30 @@ const createScene = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  //create floor
+  // const floorGeometry = new THREE.PlaneGeometry(WIDTH, HEIGHT - HEIGHT / 2);
+  // const floorMaterial = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
+  // const floorPlane = new THREE.Mesh(floorGeometry, floorMaterial);
+  // floorPlane.position.y = - 25;
+  // floorPlane.rotation.x = - Math.PI / 2;
+  // floorPlane.receiveShadow = true;
+  // scene.add(floorPlane);
+
   window.addEventListener(`resize`, handleWindowResize, false);
 
+};
+
+const addText = () => {
+
+  const title = document.createElement(`h1`);
+  title.innerHTML = `GnomeForest`;
+  title.classList.add(`Title`);
+  document.body.appendChild(title);
+
+  const explanation = document.createElement(`h1`);
+  explanation.innerHTML = `Move the gnomes to produce beautiful sounds`;
+  explanation.classList.add(`exp`);
+  document.body.appendChild(explanation);
 };
 
 const loadWithJSONLoader = url => {
@@ -79,42 +101,45 @@ const loadAssets = () => {
   return loadWithJSONLoader(`./assets/json/potgoud.json`)
     .then(geometry => {
       potgoud = new THREE.Mesh(geometry, material);
-      potgoud.scale.set(0.001, 0.001, 0.001);
+      potgoud.scale.set(0.2, 0.2, 0.2);
       potgoud.position.x = 0;
-      potgoud.rotation.x = 3;
+      potgoud.position.y = 0;
+      potgoud.position.z = 0;
+      potgoud.rotation.x = 0;
       scene.add(potgoud);
     })
     .then(() => loadWithJSONLoader(`./assets/json/fakkel.json`))
     .then(geometry => {
       fakkel = new THREE.Mesh(geometry, material);
-      fakkel.scale.set(0.001, 0.001, 0.001);
-      fakkel.position.x = 2;
+      fakkel.scale.set(0.2, 0.2, 0.2);
+      fakkel.position.x = 20;
       fakkel.rotation.x = 0;
       scene.add(fakkel);
     })
     .then(() => loadWithJSONLoader(`./assets/json/paddestoel.json`))
     .then(geometry => {
       paddestoel = new THREE.Mesh(geometry, material);
-      paddestoel.scale.set(0.001, 0.001, 0.001);
-      paddestoel.position.x = 4;
+      paddestoel.scale.set(0.2, 0.2, 0.2);
+      paddestoel.position.x = 40;
       paddestoel.rotation.x = 0;
       scene.add(paddestoel);
     })
     .then(() => loadWithJSONLoader(`./assets/json/boomstronk.json`))
     .then(geometry => {
       boomstronk = new THREE.Mesh(geometry, material);
-      const s = 0.006;
+      const s = 0.6;
       boomstronk.scale.set(s, s, s);
-      boomstronk.position.x = 3;
-      boomstronk.rotation.x = 0;
+      boomstronk.position.x = - 40;
+      boomstronk.rotation.x = - 10;
       boomstronk.rotation.y = 2;
       scene.add(boomstronk);
     })
     .then(() => loadWithJSONLoader(`./assets/json/pickaxe.json`))
     .then(geometry => {
       pickaxe = new THREE.Mesh(geometry, material);
-      pickaxe.scale.set(0.001, 0.001, 0.001);
-      pickaxe.position.x = - 2;
+      pickaxe.scale.set(0.2, 0.2, 0.2);
+      pickaxe.position.x = - 60;
+      pickaxe.position.y = 10;
       pickaxe.rotation.x = 0;
       pickaxe.rotation.y = 1;
       scene.add(pickaxe);
@@ -123,10 +148,13 @@ const loadAssets = () => {
     .then(geometry => {
       for (let i = 0;i < 5;i ++) {
         kabouter = new THREE.Mesh(geometry, material);
-        kabouter.scale.set(0.001, 0.001, 0.001);
-        kabouter.position.x = - 5.5 + (i / 2);
-        kabouter.position.y = - 2.5;
+        kabouter.scale.set(0.2, 0.2, 0.2);
+        kabouter.position.x = - 780 + (i * 70);
+        kabouter.position.y = - 350;
+        kabouter.position.z = 0;
         kabouter.rotation.x = 0;
+        kabouter.rotation.y = 0;
+        kabouter.rotation.z = 0;
         scene.add(kabouter);
         kabouters.push(kabouter);
       }
@@ -149,7 +177,7 @@ const checkCollision = () => {
 
   kabouters.forEach(kabouter => {
     const distanceToPotgoud = potgoudPos.distanceTo(kabouter.position);
-    if (distanceToPotgoud < 0.4) {
+    if (distanceToPotgoud < 40) {
       //console.log(`boom potgoud`);
       if (now - lastSynthTriggerdTime > 500) {
         synth();
