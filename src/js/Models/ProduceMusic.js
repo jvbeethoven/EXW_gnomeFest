9810867749b6f0332c3e259c83a64bcfd83e2bae
jvbeethoven <julievanbeethoven@gmail.com>
2017-12-09 14:18:01 +0100

    Two different tones #9, but can't play at once

--- src/js/Models/ProduceMusic.js
+++ src/js/Models/ProduceMusic.js
@@ -1,9 +1,34 @@
 import Tone from 'tone';
 
-const distortion = new Tone.Distortion(0.6);
-const tremolo = new Tone.Tremolo().start();
+// const polySynth = new Tone.PolySynth(4, Tone.Synth).chain(distortion, tremolo, Tone.Master);
 
-const polySynth = new Tone.PolySynth(4, Tone.Synth).chain(distortion, tremolo, Tone.Master);
+const synthA = new Tone.Synth({
+  oscillator: {
+    type: `fmsquare`,
+    modulationType: `sawtooth`,
+    modulationIndex: 3,
+    harmonicity: 3.4
+  },
+  envelope: {
+    attack: 0.001,
+    decay: 0.1,
+    sustain: 0.1,
+    release: 0.1
+  }
+}).toMaster();
+
+const synthB = new Tone.Synth({
+  oscillator: {
+    type: `triangle8`
+  },
+  envelope: {
+    attack: 2,
+    decay: 1,
+    sustain: 0.4,
+    release: 4
+  }
+}).toMaster();
+
 
 export default class ProduceMusic {
   target = 0
@@ -22,23 +47,24 @@ export default class ProduceMusic {
 
     switch (target) {
     case 1:
-      this.playDrums(distortion, tremolo, polySynth);
+      this.playDrums();
 
       break;
     case 2:
-      this.playDrums(distortion, tremolo, polySynth);
+      // this.playDrums(distortion, tremolo, polySynth);
 
       break;
     case 3:
-      this.playDrums(distortion, tremolo, polySynth);
+      // this.playDrums(distortion, tremolo, polySynth);
 
       break;
     case 4:
-      this.playDrums(distortion, tremolo, polySynth);
+      // this.playDrums(distortion, tremolo, polySynth);
 
       break;
     case 5:
-      this.playDrums(distortion, tremolo, polySynth);
+      // this.playDrums(distortion, tremolo, polySynth);
+      this.playFifth();
 
       break;
     default:
@@ -50,23 +76,24 @@ export default class ProduceMusic {
 
     switch (target) {
     case 1:
-      this.stopDrums(distortion, tremolo, polySynth);
+      this.stopDrums();
 
       break;
     case 2:
-      this.stopDrums(distortion, tremolo, polySynth);
+      // this.stopDrums(distortion, tremolo, polySynth);
 
       break;
     case 3:
-      this.stopDrums(distortion, tremolo, polySynth);
+      // this.stopDrums(distortion, tremolo, polySynth);
 
       break;
     case 4:
-      this.stopDrums(distortion, tremolo, polySynth);
+      // this.stopDrums(distortion, tremolo, polySynth);
 
       break;
     case 5:
-      this.stopDrums(distortion, tremolo, polySynth);
+      // this.stopDrums(distortion, tremolo, polySynth);
+      this.stopFifth();
 
       break;
     default:
@@ -74,13 +101,23 @@ export default class ProduceMusic {
     }
   };
 
-  playDrums = (distortion, tremolo, polySynth) => {
-    polySynth.triggerAttack([`C4`, `E4`, `G4`, `B4`]);
+  playDrums = () => {
+    // polySynth.triggerAttack([`C4`, `E4`, `G4`, `B4`]);
+    synthA.triggerAttack(`C4`);
 
   };
 
-  stopDrums = (distortion, tremolo, polySynth) => {
-    polySynth.triggerRelease([`C4`, `E4`, `G4`, `B4`]);
+  stopDrums = () => {
+    // polySynth.triggerRelease([`C4`, `E4`, `G4`, `B4`]);
+    synthA.triggerRelease();
 
+  };
+
+  playFifth = () => {
+    synthB.triggerAttack(`C1`);
+  };
+
+  stopFifth = () => {
+    synthB.triggerRelease();
   };
 }
