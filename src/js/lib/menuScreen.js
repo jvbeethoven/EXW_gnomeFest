9d365df12d3691d48cbd75feb78e1931e62f07bc
jvbeethoven <julievanbeethoven@gmail.com>
2017-12-19 17:29:44 +0100

    Fixes intro screen

--- src/js/lib/menuScreen.js
+++ src/js/lib/menuScreen.js
@@ -5,25 +5,31 @@ const menuScreen = bool => {
     menuContainer.classList.add(`menuContainer`);
     document.body.appendChild(menuContainer);
 
-    // const logo = document.createElement(`img`);
-    // logo.src = `./assets/img/logo.png`;
-    // logo.title = `logo`;
-    // logo.width = 512;
-    // logo.height = 384;
-    // logo.classList.add(`loading`);
-    // menuContainer.appendChild(logo);
-
     const menuTitle = document.createElement(`h1`);
     menuTitle.innerHTML = `GnomeFest`;
     menuTitle.classList.add(`menuTitle`);
     menuContainer.appendChild(menuTitle);
+
+    const menuImg = document.createElement(`img`);
+    menuImg.src = `/assets/img/startscreenimg.png`;
+    menuImg.classList.add(`menuImg`);
+    menuContainer.appendChild(menuImg);
+
+    const explanationContainer = document.createElement(`div`);
+    explanationContainer.classList.add(`explanationContainer`);
+    menuContainer.appendChild(explanationContainer);
+
+    const explanation = document.createElement(`h1`);
+    explanation.innerHTML = `Collide the gnomes with objects by dragging them so you can experience beautiful sounds. Preferably used on Google Chrome v63.0`;
+    explanation.classList.add(`explanation`);
+    explanationContainer.appendChild(explanation);
 
     const button = document.createElement(`button`);
     button.innerHTML = `explore`;
     button.id = `button`;
     button.value = `explore`;
     button.classList.add(`button`);
-    menuContainer.appendChild(button);
+    explanationContainer.appendChild(button);
   } else {
     const elem = document.querySelector(`.menuContainer`);
     elem.classList.add(`hide`);
